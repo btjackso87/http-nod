e0083f9dc6cc692b78f3ce6ba8218148ae336ccb
Brian Jackson <btjackso091987@gmail.com>
2023-01-19 17:39:39 -0500

    update server

--- server.mjs
+++ server.mjs
@@ -1,6 +1,16 @@
 import { createServer } from 'http';
+import {fs} from 'fs';
 
 createServer((req, res) => {
-  res.write('Hello Wor');
-  res.end();
+ res.writeHead(200, {'Content-Type': 'text/html'})
+fs.readFile('index.html', function(error, data){
+    if(error) {
+        res.writeHead(404)
+        res.write('error: file not found')
+    } else {
+        res.write(data)
+    }
+    res.end()
+})
+    
 }).listen(process.env.PORT);
