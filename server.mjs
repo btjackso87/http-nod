390bf52867581b52300682ad41a14045e2b4bbe3
Brian Jackson <btjackso091987@gmail.com>
2023-01-19 17:21:12 -0500

    edit server

--- server.mjs
+++ server.mjs
@@ -1,29 +1,6 @@
-const http = require('http')
-const fs = require('fs')
-const port = process.env.PORT || 5000
+import { createServer } from 'http';
 
-
-const server = http.createServer(function(req, res){
-    res.writeHead(200, {'Content-Type': 'text/html'})
-    fs.readFile('index.html', function(error, data){
-        if(error) {
-            res.writeHead(404)
-            res.write('error: file not found')
-        } else {
-            res.write(data)
-        }
-
-        res.end()
-    })
-
-
-    
-})
-
-server.listen(port, function(error){
-    if(error) {
-        console.log('something went wrong', error)
-    } else {
-        console.log('server is listening on port' + port)
-    }
-})
+createServer((req, res) => {
+  res.write('Hello World!');
+  res.end();
+}).listen(process.env.PORT);
